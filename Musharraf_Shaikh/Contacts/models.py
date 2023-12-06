e828688e6c8cdaeb74183af1ca568f3354cafccc
musharraf3 <musharrafshaikh1850@gmail.com>
2023-12-05 16:04:28 -0800

    Contact Form

--- Musharraf_Shaikh/Contacts/models.py
+++ Musharraf_Shaikh/Contacts/models.py
@@ -1,11 +1,16 @@
 from django.db import models
 from django.utils import timezone
 
+
 # Create your models here.
 class Contacts(models.Model):
-    name = models.CharField(max_length=20, null=False)
-    description = models.TextField(max_length=200, default="")
-    is_done = models.BooleanField(default=False)
-    email = models.EmailField()
-    created_time = models.DateTimeField(auto_now_add=True, editable=False)
-    notes = models.TextField(max_length=200, default="")
+    name = models.CharField(max_length=20, null=False, unique=True)
+    phone_number = models.CharField(max_length=12,null=False,default='+1')
+    email = models.EmailField(unique=True)
+    notes = models.CharField(max_length=50)
+    created_time = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        if not self.created_time:
+            self.created_time = timezone.now()
+        super(Contacts, self).save(*args, **kwargs)
