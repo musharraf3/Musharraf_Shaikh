9335313c9fabd2a9e41bd68bf77cd6032bb6883b
musharraf3 <musharrafshaikh1850@gmail.com>
2023-12-07 20:59:22 -0800

    Final Web App

--- Musharraf_Shaikh/Contacts/models.py
+++ Musharraf_Shaikh/Contacts/models.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 # Create your models here.
 class Contact(models.Model):
     name = models.CharField(max_length=20, null=False, unique=True)
-    phone_number = models.CharField(max_length=12,null=False,default='+1', unique=True)
+    phone = models.CharField(max_length=12,null=False,default='+1', unique=True)
     email = models.EmailField(unique=True)
     notes = models.CharField(max_length=50, default=' ')
     created_time = models.DateTimeField(auto_now_add=True)
